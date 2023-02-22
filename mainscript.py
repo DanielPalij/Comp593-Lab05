428802061a5927e059b53c6049a3cf517e99f6bf
DanielPalij <daniel.palij@flemingcollege.ca>
2023-02-21 23:18:59 -0500

    Update mainscript.py

--- mainscript.py
+++ mainscript.py
@@ -3,19 +3,15 @@ import poke_api
 import api_pastebin
 
 def main():
-    data = poke_api.get_pokemon(argv[1])
-
+   
+def get_search_term():
     
-    ability1 = data['abilities'][0]
-    ability2 = data['abilities'][1]
-    
-    api_pastebin.post_new_paste(argv[0], )
-
-    print(data['abilities'][0])
 
 
-main()
+   
 
+if __name__ == '__main__':
+    main()
 
 
 
